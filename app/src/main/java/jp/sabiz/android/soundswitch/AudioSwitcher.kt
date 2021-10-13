e26d80f251479618381ca270b1e95e70c7f7db56
sAbIz <bellett@sabiz.jp>
2021-10-13 21:35:32 +0900

    more simply implementation

--- app/src/main/java/jp/sabiz/android/soundswitch/AudioSwitcher.kt
+++ app/src/main/java/jp/sabiz/android/soundswitch/AudioSwitcher.kt
@@ -7,34 +7,13 @@ import android.media.AudioManager
 class AudioSwitcher internal constructor(context: Context) {
 
     private val mAudioManager: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-    private val mStorage: DataStorage = DataStorage(context, STORAGE_NAME)
-
-    companion object {
-
-        private const val STORAGE_NAME = "AUDIO_SWITCHER_DATA"
-        private val STREAM_TABLE = arrayOf(AudioManager.STREAM_ALARM, AudioManager.STREAM_DTMF,
-                                            AudioManager.STREAM_NOTIFICATION, AudioManager.STREAM_SYSTEM,
-                                            AudioManager.STREAM_VOICE_CALL,AudioManager.STREAM_MUSIC)
-    }
 
     private fun unmute() {
-        STREAM_TABLE.forEach {
-            val volume = mStorage.restore(it,mAudioManager.getStreamMaxVolume(it))
-            if (it == AudioManager.STREAM_VOICE_CALL) {
-                mAudioManager.setStreamVolume(AudioManager.STREAM_RING,volume, AudioManager.FLAG_SHOW_UI)
-            }
-            mAudioManager.setStreamVolume(it,volume, AudioManager.FLAG_SHOW_UI)
-        }
+        mAudioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI)
     }
 
     private fun mute() {
-        STREAM_TABLE.forEach {
-            mStorage.store(it,mAudioManager.getStreamVolume(it))
-            if (it == AudioManager.STREAM_VOICE_CALL) {
-                mAudioManager.setStreamVolume(AudioManager.STREAM_RING,0, AudioManager.FLAG_SHOW_UI)
-            }
-            mAudioManager.setStreamVolume(it,0, AudioManager.FLAG_SHOW_UI)
-        }
+        mAudioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI)
     }
 
     fun vibe() {

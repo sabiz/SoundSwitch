1d56bbd68807cac45e834e2baf44cf2f40c83ee2
sAbIz <bellett@sabiz.jp>
2022-01-18 20:00:19 +0900

    improve mute action

--- app/src/main/java/jp/sabiz/android/soundswitch/AudioSwitcher.kt
+++ app/src/main/java/jp/sabiz/android/soundswitch/AudioSwitcher.kt
@@ -6,24 +6,33 @@ import android.media.AudioManager
 
 class AudioSwitcher internal constructor(context: Context) {
 
+    private companion object {
+
+        private const val STORAGE_NAME = "AUDIO_SWITCHER_DATA"
+        private val STREAM_TABLE = arrayOf(AudioManager.STREAM_ALARM, AudioManager.STREAM_DTMF,
+            AudioManager.STREAM_NOTIFICATION, AudioManager.STREAM_SYSTEM,
+            AudioManager.STREAM_VOICE_CALL,AudioManager.STREAM_MUSIC
+            ,AudioManager.STREAM_RING)
+    }
+
     private val mAudioManager: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-
-    private fun unmute() {
-        mAudioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI)
-    }
-
-    private fun mute() {
-        mAudioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI)
-    }
+    private val mStorage: DataStorage = DataStorage(context, STORAGE_NAME)
 
     fun vibe() {
-        mute()
+        for (stream in STREAM_TABLE) {
+            mStorage.store(stream,mAudioManager.getStreamVolume(stream))
+        }
+        mAudioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI)
         mAudioManager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
     }
 
     fun normal() {
-        unmute()
+        mAudioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI)
         mAudioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+        for (stream in STREAM_TABLE) {
+            val volume = mStorage.restore(stream,mAudioManager.getStreamMaxVolume(stream))
+            mAudioManager.setStreamVolume(stream,volume, AudioManager.FLAG_SHOW_UI)
+        }
 
     }
 }
